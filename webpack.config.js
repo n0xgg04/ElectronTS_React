8191b0c5ed78f695a1a882776e31a7c3a44c291c
n0xgg04 <dulieu.vblc@gmail.com>
2023-10-19 20:15:39 +0700

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = [
         },
         plugins: [
             new HtmlWebpackPlugin({
-                template: './src/index.html'
+                template: './build/index.html'
             })
         ]
     }
